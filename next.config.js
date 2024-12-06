b772ba04ad78211830bb3576eb5ee9455097d07a
dobe4ever <helsola8@gmail.com>
2024-12-06 07:24:13 +0700

    auto refresh after push/deploy changes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   webpack: (config, { dev, isServer }) => {
+//     config.optimization.minimize = false;
+//     return config;
+//   },
+// }
+
+// module.exports = nextConfig
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
