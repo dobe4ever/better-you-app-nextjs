db1542bf5dbb2e311733cc26e2e9a6054cd5b684
dobe4ever <helsola8@gmail.com>
2024-12-06 08:07:07 +0700

    z

--- components/layout/widgets-container/achievements-widget/achievements-widget.tsx
+++ components/layout/widgets-container/achievements-widget/achievements-widget.tsx
@@ -5,7 +5,7 @@ export function AchievementsWidget() {
   return (
     <Widget title="Badges" className="flex flex-col gap-2">
       <Trophy size={32} />
-      <div className="text-sm text-gray-600 mt-6">
+      <div className="text-sm text-gray-600 mt-5">
       Earn rewards by reaching milestones
       </div>
     </Widget>
