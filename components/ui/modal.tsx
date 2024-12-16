265d2189cdff60420d8e51f89a887e30f5d2bc3c
dobe4ever <helsola8@gmail.com>
2024-12-17 04:37:02 +0700

    z

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -11,7 +11,7 @@ interface ModalProps {
 export const Modal: React.FC<ModalProps> = ({ onClose, children }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white pt-2 pl-1 rounded-lg w-full h-full">
+      <div className="bg-white pt-2 pl-1 w-full min-h-screen">
         <ChevronLeftBtn onClick={onClose}/>
         <TopNav />
         {/* {children} */}
