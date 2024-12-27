d0c8111a203342b01fb2201449489b87a15a51a3
dobe4ever <132445266+dobe4ever@users.noreply.github.com>
2024-12-28 03:15:17 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,11 +76,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={`${inter.className} ${nunito.className}`}>{children}</body>
     </html>
   )
 }
-
